753f04a20d36d2b29d753c914d3f914daea40f60
Arjun Gill <arjun.gill@berkeley.edu>
2017-01-25 10:20:26 -0800

    Added readPlanets method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,26 @@
 public class NBody {
+	public static double readRadius(String filename) {
+		In in = new In(filename);
+		in.readInt();
+		return in.readDouble();
+	}
 
+	public static Planet[] readPlanets(String filename) {
+		In in = new In(filename);
+		Planet[] planetList = new Planet[5];
+		
+		int numPlanets = in.readInt();
+		in.readDouble();
+		for (int i = 0; i < numPlanets; i++) {
+			double xxPos = in.readDouble();
+			double yyPos = in.readDouble();
+			double xxVel = in.readDouble();
+			double yyVel = in.readDouble();
+			double mass = in.readDouble();
+			String img = in.readString();
+			planetList[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, img);
+		}
+		
+		return planetList;
+	}
 }
