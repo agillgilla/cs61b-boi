70782b48d0426e7cc07c817d10997a9dafa79f5f
Arjun Gill <arjun.gill@berkeley.edu>
2017-01-27 12:21:01 -0800

    Completed Lab 2, Corrected NullPointerException

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,6 +80,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null || B == null) {
+            return null;
+        }
         IntList C = A;
         while (A.rest != null) {
             A = A.rest;
