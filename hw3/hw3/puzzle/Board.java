97c5b967ae4f5f25b986da9008342e3641f6c017
Arjun Gill <agillgilla@gmail.com>
2017-03-23 15:16:34 -0700

    Added neighbors() and fixed correctI and correctJ

--- hw3/hw3/puzzle/Board.java
+++ hw3/hw3/puzzle/Board.java
@@ -1,9 +1,14 @@
 package hw3.puzzle;
 
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.ArrayDeque;
+
 public class Board implements WorldState {
 
     private int[][] tiles;
     private int N;
+    private static int BLANK = 0;
     /**
      * Constructs a board from an N-by-N array of tiles where
      * tiles[i][j] = tile at row i, column j
@@ -40,7 +45,37 @@ public class Board implements WorldState {
      * @return
      */
     public Iterable<WorldState> neighbors() {
-        return null;
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
     }
 
     /**
@@ -69,7 +104,7 @@ public class Board implements WorldState {
         int manDistSum = 0;
         for (int i = 0; i < this.N; i++) {
             for (int j = 0; j < this.N; j++) {
-                if (!(i == (this.N - 1) && j == (this.N - 1)) && this.tileAt(i, j) > 0 && this.tileAt(i, j) < this.N - 1) {
+                if (!(i == (this.N - 1) && j == (this.N - 1)) && this.tileAt(i, j) >= 0 && this.tileAt(i, j) < this.N) {
                     manDistSum += Math.abs(i - correctI(this.tileAt(i, j)));
                     manDistSum += Math.abs(j - correctJ(this.tileAt(i, j)));
                 }
@@ -145,10 +180,16 @@ public class Board implements WorldState {
     }
 
     public int correctI(int x) {
+        if (x == 0) {
+            return this.N - 1;
+        }
         return (x - 1) / this.N;
     }
 
     public int correctJ(int x) {
+        if (x == 0) {
+            return this.N - 1;
+        }
         return (x - 1) % this.N;
     }
 
